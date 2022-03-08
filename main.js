c490d9f0c786f930cc3da0e42bbdcadcc61e97b6
georgesarreas <george.sarreas@gmail.com>
2022-03-08 16:04:50 +0200

    some touble populating grid

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+//populate the container\
+function createGrid(x) {
+    x=16;
+    for (var rows=0; rows <x ; rows++){
+        for (var columns = 0; columns <x ; columns++){
+            $(".container").append("<div class="grid"></div>");
+        };
+    };
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
+}
 
